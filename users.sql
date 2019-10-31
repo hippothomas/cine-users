856db4f436850c1c48390d153c52413f795c3ef0
hippothomas <hippolyte.thomas1@gmail.com>
2019-10-31 16:13:10 +0100

    Ajout du fichier d'import sql

---
+++ users.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 31 oct. 2019 à 15:11
+-- Version du serveur :  5.7.23
+-- Version de PHP :  5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `cine-users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last-name` varchar(255) NOT NULL,
+  `first-name` varchar(255) NOT NULL,
+  `age` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `last-name`, `first-name`, `age`) VALUES
+(1, 'Thomas', 'Hippolyte', 20),
+(2, 'Chauveau', 'Bastien', 20),
+(3, 'Caillaud', 'Hugo', 19),
+(4, 'Galerne', 'Valentin', 20),
+(5, 'Dandouau', 'Alexandre', 21);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
